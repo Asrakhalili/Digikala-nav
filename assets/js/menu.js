7254a1d7fcbd131606d0f5a7bea40e98d3d55aa1
Asrakhalili <asraokhalili@gmail.com>
2023-11-27 12:44:22 +0330

    end mobile items in xs

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,69 +1,69 @@
 
-// $(document).ready(function(){
-// $('.menu-link').mouseenter(function(
-// ){
-//     if($(this).is(':hover')){
-//         $('.bg-menu').show()
-//         $('body').css('overflow', 'hidden');
-//         $('.menu-item').first().addClass("mobile-hover")
-//         $('.search').hide()
-//         $('.bg').hide()
+$(document).ready(function(){
+$('.menu-link').mouseenter(function(
+){
+    if($(this).is(':hover')){
+        $('.bg-menu').show()
+        $('body').css('overflow', 'hidden');
+        $('.menu-item').first().addClass("mobile-hover")
+        $('.search').hide()
+        $('.bg').hide()
 
-//     }
+    }
     
 
-// })
+})
 
-// $('.menu-items').mouseenter(function(){
-//     if($(this).is(':hover')){
-//         $('.bg-menu').show() 
-//         $('.menu-item').first().addClass("mobile-hover")
-//     }
+$('.menu-items').mouseenter(function(){
+    if($(this).is(':hover')){
+        $('.bg-menu').show() 
+        $('.menu-item').first().addClass("mobile-hover")
+    }
     
 
-// })
+})
 
-// $('.menu').mouseleave(function(){
-//     if(!$(this).is(':hover')){
-//         $('.bg-menu').hide() 
-//     }
+$('.menu').mouseleave(function(){
+    if(!$(this).is(':hover')){
+        $('.bg-menu').hide() 
+    }
     
 
-// })
-// $('.menu-link').mouseleave(function(){
-//     if(!$(this).is(':hover')){
-//         $('.bg-menu').hide() 
-//     }
+})
+$('.menu-link').mouseleave(function(){
+    if(!$(this).is(':hover')){
+        $('.bg-menu').hide() 
+    }
     
 
-// })
+})
 
 
 
 
 
 
-// })
-// $(document).ready(function(){
-//     $('.menu-item').eq(1).hover(function(){
-//             $('.menu-item').first().removeClass("mobile-hover") ;
-//             $('.elec-item').show();
-//                $('.mobile-item').hide()
+})
+$(document).ready(function(){
+    $('.menu-item').eq(1).hover(function(){
+            $('.menu-item').first().removeClass("mobile-hover") ;
+            $('.elec-item').show();
+               $('.mobile-item').hide()
     
-//     })
-// })
-// $(document).ready(function(){
-//     $('.menu-item').eq(0).hover(function(){
-//      $('.mobile-item').show()
-//      $('.elec-item').hide();
-//     })
-// })
+    })
+})
+$(document).ready(function(){
+    $('.menu-item').eq(0).hover(function(){
+     $('.mobile-item').show()
+     $('.elec-item').hide();
+    })
+})
 
-// $(document).ready(function(){
-//     $('.menu-link').click(function(e){
-// e.preventDefault()
-//     })
-// })
+$(document).ready(function(){
+    $('.menu-link').click(function(e){
+e.preventDefault()
+    })
+})
 
 // start nav down jq
 $(document).ready(function(){
